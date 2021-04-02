2b83ef3b1d57a66e58e3c5a33b6c971b27a97113
Laurent Doguin <laurent.doguin@gmail.com>
2021-04-02 07:36:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Run parse-server on Clever Cloud
+
+How to run the BaaS [parse-server](https://github.com/parse-community/parse-server) on [Clever CLoud](https://clever-cloud.com/).
+
+This parse-server instance will be configured to use PostgreSQL, Redis and S3.
+
+Assuming you have [clever-tools](https://www.clever-cloud.com/doc/getting-started/cli/) installed and configured:
+
+
+```
+git clone https://github.com/parse-community/parse-server
+cd parse-server
+clever create --type node myParseServer
+clever addon create postgresql-addon --plan xs_sml --region par myParseServerPG
+clever service link-addon myParseServerPG
+clever addon create redis-addon --plan dev --region par myParseServerRedis
+clever service link-addon myParseServerRedis
+clever addon create cellar-addon --plan s --region par myParseServerCellar
+clever service link-addon myParseServerCellar
+# now would be a good time to open the cellar addon and create a bucket
+clever env set CELLAR_BUCKET yourBucket
+clever env set SERVER_URL https://yourdomain.cleverapps.io/parse
+clever env set MASTER_KEY yourMasterKey
+clever env set JAVASCRIPT_KEY yourJSKey
+clever domain add yourdomain.cleverapps.io
+clever deploy
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+
+var RedisCacheAdapter = require('parse-server').RedisCacheAdapter;
+var redisURL = process.env.REDIS_URL || "redis://localhost:6379";
+var redisOptions = {url: redisURL}
+var redisCache = new RedisCacheAdapter(redisOptions);
+
+var S3Adapter = require("@parse/s3-files-adapter");
+var AWS = require("aws-sdk");
+
+var s3Options = {
+  bucket: process.env.CELLAR_BUCKET,
+  s3overrides: {
+    accessKeyId: process.env.CELLAR_ADDON_KEY_ID,
+    secretAccessKey: process.env.CELLAR_ADDON_KEY_SECRET,
+    endpoint: process.env.CELLAR_ADDON_HOST
+  }
+};
+
+var s3Adapter = new S3Adapter(s3Options);
+
+var postgresqlURI = process.env.POSTGRESQL_ADDON_URI;
+postgresqlURI = postgresqlURI.slice(0, 8) + postgresqlURI.slice(10, postgresqlURI.length);
+
+var config = {
+  databaseURI: postgresqlURI,
+  appId: process.env.APP_ID || 'APPLICATION_ID',
+  masterKey: process.env.MASTER_KEY || 'MASTER_KEY',
+  appName: process.env.APP_NAME || "MyTest",
+  cloud: process.env.CLOUD_FOLDER || "./cloud/main",
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse",
+  logLevel: process.env.LOG_LEVEL || "info",
+  javascriptKey : process.env.JAVASCRIPT_KEY || 'JAVASCRIPT_KEY',
+  cacheAdapter: redisCache,
+  filesAdapter: s3Adapter,
+};
+var api = new ParseServer(config);
+
+
+var app = express();
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
+app.use('/public', express.static(path.join(__dirname, '/public')));
+// Listen for connections on port 1337
+var port = process.env.PORT || 1337;
+app.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+module.exports = {
+  app,
+  config,
+};
